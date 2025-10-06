44efbae8b5018bc097f8df2ed10541fddd59c539
larossepr <larossekg@gmail.com>
2025-10-06 17:20:57 +0000

    "typo arreglado"

--- encuestador_web/pages/encuestador_web.py
+++ encuestador_web/pages/encuestador_web.py
@@ -56,9 +56,9 @@ with form.form(key=f"encuesta_form_{st.session_state.form_id}"):
     profesion = st.text_input("Profesion :")
     sector_laboral = st.radio("Sector Laboral:", ["Publico", "Privado", "Independiente", "Desempleado"], horizontal=True)
     ocio = ["Bares", "Colmados", "Galleras", "Play", "Billar", "Playa o rio"]
-    lugares_ocio = st.multiselect("Lugares que visita la gente en la comunidad para divertirse    || ELija uno o mas lugares ||", ocio, placeholder="Elija uno o mas lugares")
+    lugares_ocio = st.multiselect("Lugares que visita la gente en la comunidad para divertirse    || Elija uno o mas lugares ||", ocio, placeholder="Elija uno o mas lugares")
     orgs = ["Iglesia", "Grupo Deportivo", "Club", "Asociacion de Agricultores", "Asociacion de Comerciantes", "Asociacion de Ganaderos", "Otra"]
-    organizacion = st.multiselect("Organizaciones a las que pertenece    || ELija una o mas organizaciones ||", orgs, placeholder="Elija una o mas organizaciones")
+    organizacion = st.multiselect("Organizaciones a las que pertenece    || Elija una o mas organizaciones ||", orgs, placeholder="Elija una o mas organizaciones")
     tierra = st.radio("Posee usted tierra?", ["Si", "No"], horizontal=True, index=1)
     vivienda = st.radio("Posee usted vivienda?", ["Si", "No"], horizontal=True, index=1)
     exitencia = st.radio(f"Sabia usted de la existencia del proyecto {proyecto}?", ["Si", "No"], horizontal=True, index=0)
